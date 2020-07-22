cf896fae9a96183fd2cc2806d968a30a57b47088
jun9130 <jun.nakama1030@gmail.com>
2020-07-22 13:55:10 +0900

    lesson

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(1); 
+let people = {
+  list: {
+    japansese: {
+      man: [
+        { name: "ヤマト", age: 20, teachProgramming() {} },
+        { name: "タケル", age: 32, teachHistory() {} },
+      ],
+      teach() {}
+    },
+    America: {
+      man: [ { name: "TOM" }, {name: "MIKE" } ],
+      makeSmile() {}
+    }
+  },
+  plan() {},
+  greeting() {}
+};
+
+people.list.japansese.man[0].teachProgramming();
