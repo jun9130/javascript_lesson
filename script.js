7030fd3740304b6f1eb71b8d91dd3b1f72b7f7f3
jun9130 <jun.nakama1030@gmail.com>
2020-07-23 15:58:44 +0900

    関数

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-let people = {
-  list: {
-    japansese: {
-      man: [
-        { name: "ヤマト", age: 20, teachProgramming() {} },
-        { name: "タケル", age: 32, teachHistory() {} },
-      ],
-      teach() {}
-    },
-    America: {
-      man: [ { name: "TOM" }, {name: "MIKE" } ],
-      makeSmile() {}
-    }
-  },
-  plan() {},
-  greeting() {}
-};
+// let people = {
+//   list: {
+//     japansese: {
+//       man: [
+//         { name: "ヤマト", age: 20, teachProgramming() {} },
+//         { name: "タケル", age: 32, teachHistory() {} },
+//       ],
+//       teach() {}
+//     },
+//     America: {
+//       man: [ { name: "TOM" }, {name: "MIKE" } ],
+//       makeSmile() {}
+//     }
+//   },
+//   plan() {},
+//   greeting() {}
+// };
 
 // window.console.log();
-
 // window.alert();
+// window.document.getElementById();
 
-// window.document.getElementById()
+
+// 関数の例
+// returnありの場合
+function isTweetable(text) {
+  return text.length <= 140;
+}
+
+console.log(isTweetable("foo"));
+
+// returnなしの場合
+function alertTweetable(text) {
+  if (text.length <= 140) {
+    alert("you can tweet!");
+  }
+}
+alertTweetable("aaa")
