fdafb7ae8dccd06e625aabdb5a959a90f114ee0a
jun9130 <jun.nakama1030@gmail.com>
2020-08-13 13:46:40 +0900

    オブジェクト生成 配列

--- script.js
+++ script.js
@@ -7,5 +7,19 @@ const str = '文字列';
 console.log(`これは${str}です`);
 
 const foo = null;
-console.log(foo); // => null
+console.log(foo);
+
+//オブジェクト生成
+const obj = {
+  key: 'value'
+};
+console.log(obj.key);
+
+//配列
+const array = ['1番目', '2番目', '3番目'];
+console.log(array[0]);
  
+
+console.log(100 > 1);
+console.log(100 < 1);
+
