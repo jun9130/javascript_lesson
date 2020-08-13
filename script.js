00238c1dc2527ca8bef54c0a89bb78dfaf7a0c55
jun9130 <jun.nakama1030@gmail.com>
2020-08-13 14:54:56 +0900

    演算子

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-const total = 32 * 2;
-console.log(total);
-
-console.log('複数の改行\nを入れ\nたい');
-
-const str = '文字列';
-console.log(`これは${str}です`);
-
-const foo = null;
-console.log(foo);
-
-//オブジェクト生成
-const obj = {
-  key: 'value'
-};
-console.log(obj.key);
-
-//配列
-const array = ['1番目', '2番目', '3番目'];
+//分割代入
+const array = [1, 2];
+const [a, b] = array;
 console.log(array[0]);
- 
 
-console.log(100 > 1);
-console.log(100 < 1);
-
+const obj = {
+  key: "value"
+};
+//オブジェクトの生成は{ }で囲む
+const { key } = obj;
+console.log(key);
