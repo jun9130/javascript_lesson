7bbeeb3277606a342bef30c358237206e5ed63f2
jun9130 <jun.nakama1030@gmail.com>
2020-08-15 19:33:23 +0900

    繰り返し

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-//if文
-if (true) {
-  console.log('trueなので実行される')
+//while文
+let x = 0;
+console.log(`${x}です。`);
+while (x < 10) {
+  console.log(x);
+  x += 1;
 }
+console.log('ループ終了です');
 
-const country = "アメリカ"
-switch (country) {
-  case "日本":
-    document.write("日本です");
-    break; //break文でswitch文から抜ける
-  case "イタリア":
-    document.write("イタリアです");
-    break;
-  case "アメリカ":
-    document.write("アメリカです");
-    break;
-  default:
-    document.write("日本でもイタリアでもアメリカでもありません。");
-    break;
+//for文
+let total = 0;
+for (let i = 0; i < 5; i++) {
+  total += i + 1;
 }
+console.log(total)
 
+//forEachメソッド
+const array = [1, 2, 3];
+array.forEach(currentValue => {
+    console.log(currentValue);
+});
