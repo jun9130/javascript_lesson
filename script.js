b7e4aedd5fd32b22ce7812d484616ce3b4ff4614
jun9130 <jun.nakama1030@gmail.com>
2020-08-18 22:40:44 +0900

    配列

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-//インデックスの取得
-const pro = ['ruby', 'PHP', 'JavaScript', 'Go'];
-const indexOfJS = pro.indexOf('JavaScript');
-document.write(`配列の${indexOfJS + 1}番目には${pro[2]}が入っています`);
+//プロトタイプオブジェクト
+const obj = {}; //作成時にObject.prototype`に定義されたものを継承
+console.log(obj.toString === Object.prototype.toString); //=>trueになる
 
-//条件に一致する要素の取得
-const colors = [
-  {color: "red"},
-  {color: "blue"},
-  {color: "yellow"}
-];
-const blueColor = colors.find((obj) => {
-  return obj.color === "blue";
-});
-console.log(blueColor);
 
-//指定範囲の要素を取得
-const array =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-console.log(array.slice(1, 4));
-console.log(array.slice(1));
-console.log(array.slice(-1));
-console.log(array.slice(4, 1));
+//
+const array = ['PHP', 'Ruby', 'JavaScript'];
+if (array.includes('JavaScript')) {
+  window.document.write(`配列に${array[2]}は含まれている`);
+}
