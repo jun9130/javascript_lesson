bfa98819467241c01c3d71169b3bfb16ef0e119f
jun9130 <jun.nakama1030@gmail.com>
2020-08-20 23:14:12 +0900

    lesson

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ const text = document.getElementById('text')
 btn.addEventListener('click', function(){
   text.style.color='red';
   text.innerHTML = "クリックされました";
+  const input = window.document.getElementById('input');
+  input.value = "書き換え"
 });
+
