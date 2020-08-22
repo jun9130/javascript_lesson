eccc007afd6e9fa4e4d6856daff940f03ed7d22c
jun9130 <jun.nakama1030@gmail.com>
2020-08-22 23:25:04 +0900

    lesson

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
   const newDiv = document.createElement("div");
-  const newText = document.createTextNode('追加しました');
+  const newText = document.createTextNode('クリックで追加');
   newDiv.appendChild(newText);
   btn.parentElement.insertBefore(newDiv, btn.nextSibling);
 });
