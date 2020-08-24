7c6723ec45bb91b15771061fee9c6000308508f5
jun9130 <jun.nakama1030@gmail.com>
2020-08-25 07:08:24 +0900

    lesson

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const text = document.getElementById('text');
-const btn = document.getElementById('btn');
+// const btn = document.getElementById('btn');
 
-btn.addEventListener('click', function(){
-  const newDiv = document.createElement("div");
-  const newText = document.createTextNode('クリックで追加');
-  newDiv.appendChild(newText);
-  btn.parentElement.insertBefore(newDiv, btn.nextSibling);w
-});
+// btn.addEventListener('click', function(){
+//   const text = document.getElementById('text');
+//   const newDiv = document.createElement("div");
+//   const text = document.getElementById('text');
+//   const newText = document.createTextNode('クリックで追加');
+//   newDiv.appendChild(newText);
+//   btn.parentElement.insertBefore(newDiv, btn.nextSibling);
+// });
+
+const inputText = document.getElementById('inputText');
+console.log(inputText.value);
