c0eb2990acc45f9719ea1e4ca1c0145990244278
jun9130 <jun.nakama1030@gmail.com>
2020-08-30 22:43:01 +0900

    関数

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-// const btn = document.getElementById('btn');
-
-// btn.addEventListener('click', function(){
-//   const text = document.getElementById('text');
-//   const newDiv = document.createElement("div");
-//   const text = document.getElementById('text');
-//   const newText = document.createTextNode('クリックで追加');
-//   newDiv.appendChild(newText);
-//   btn.parentElement.insertBefore(newDiv, btn.nextSibling);
-// });
-
-const inputText = document.getElementById('inputText');
-console.log(inputText.value);
+function isTweetable(text) {
+  return text.length <= 5;
+}
+console.log(isTweetable("foo")); //true
+console.log(isTweetable("fooooo")); //false
--- todo/todo.js
+++ todo/todo.js
@@ -1,1 +1,13 @@
-let btn = document.getElementById('btn')
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+  const text = document.getElementById('text');
+  const newDiv = document.createElement("div");
+  const text = document.getElementById('text');
+  const newText = document.createTextNode('クリックで追加');
+  newDiv.appendChild(newText);
+  btn.parentElement.insertBefore(newDiv, btn.nextSibling);
+});
+
+const inputText = document.getElementById('inputText');
+console.log(inputText.value);
