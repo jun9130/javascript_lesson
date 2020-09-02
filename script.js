a7189979594c6a617ca6bd14c85092bf07845f84
jun9130 <jun.nakama1030@gmail.com>
2020-09-02 22:59:28 +0900

    コールバック関数

--- script.js
+++ script.js
@@ -6,6 +6,30 @@ function unfloow() {
   console.log('フォローを外しました');
 }
 
-function cancelTweet() {
+const cancelTweet = function cancelTweet() {
   console.log('ツイートをキャンセルしました')
 }
+
+function confirmed(fn) {
+  if(window.confirm("実行しますか?")) {
+    fn();
+  }
+}
+
+confirmed(unfloow);
+
+// const unfollow = function () {
+//   console.log('フォローを外しました');
+// }
+
+// const cancelTweet = function () {
+//   console.log('ツイートをキャンセルしました')
+// }
+
+// function confirmed(fn) {
+//   if(window.confirm("実行しますか?")) {
+//     fn();
+//   }
+// }
+
+// confirmed(cancelTweet);
