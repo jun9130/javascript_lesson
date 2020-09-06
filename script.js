7a5f2942fcb577dc54b1b3c2fac3692afcb694c8
jun9130 <jun.nakama1030@gmail.com>
2020-09-06 23:33:26 +0900

    lesson

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-function num() {
-  number = window.prompt('数字を入れてください')
-  console.log(`${number}の10倍は${parseInt(number) * 10}です`);
-}
+const array = [];
 
-num();
+const num = array.push(window.prompt().split(' '));
 
-
-
-
+console.log(num);
