fde7cd6ea93e33e62e24aa1b0aad45bd83950e67
jun9130 <jun.nakama1030@gmail.com>
2020-11-26 22:49:52 +0900

    lesson

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 //   window.document.write("<div>こんにちは</div>")
 // }
 
-function changeColor(newColor) {
-  const elem = document.getElementById('para');
-  elem.style.color = newColor;
-}
+// function changeColor(newColor) {
+//   const elem = document.getElementById('para');
+//   elem.style.color = newColor;
+// }
+
+console.log("it's me!");
+console.log('it\'s me!');
+
 
