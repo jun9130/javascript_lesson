57fd9b16a5ce154c323576b9d913fe306d93cd56
jun9130 <jun.nakama1030@gmail.com>
2020-12-03 07:21:32 +0900

    lesson

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// const buttonList = document.querySelectorAll('button');
-// [...buttonList].forEach((button) => {
-//   button.addEventListener('click', ()=> {
-//     const li = button.closest('li');
-//     li.classList.add('done');
-//   })
-// });
-
-const button = document.querySelector('button');
-button.addEventListener('click', ()=> {
-  const div = document.querySelector('div');
-  div.classList.add('change');
-})
+const button = document.getElementById('add-button');
