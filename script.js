cae72f567f5ae0afd03065cfbc4486bc74ced041
jun9130 <jun.nakama1030@gmail.com>
2020-12-04 13:19:04 +0900

    lesson

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const button = document.getElementById('add-button');
+button.addEventListener('click', ()=> {
+  const input = document.getElementById('new-todo');
+  
+  const li = document.createElement('li');
+  li.innerText = input.value;
+
+  const doneButton = document.createElement('button');
+  doneButton.innerText = 'done';
+  li.appendChild(doneButton);
+  
+  const ul = document.querySelector('ul');
+  ul.appendChild(li);
+});
