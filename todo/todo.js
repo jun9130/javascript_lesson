ab23916f322b26ee6d8fc18becba1d0dda2afb60
jun9130 <jun.nakama1030@gmail.com>
2020-12-05 14:49:00 +0900

    途中

--- todo/todo.js
+++ todo/todo.js
@@ -1,9 +1,17 @@
 const addButton = window.document.getElementById('addButton');
 addButton.addEventListener('click', ()=> {  //アロー関数
   const input = window.document.querySelector('input'); //input要素取得
-  const li = window.document.createElement('li'); //liタグ作成
   const ul = window.document.querySelector('ul'); //ul要素取得
+
+  const li = window.document.createElement('li'); //liタグ作成
   li.textContent = input.value; //入力されたテキストをliのテキストに反映
   ul.appendChild(li); //ulタグの最後に追加
-  
+
+  const doneButton = window.document.createElement('button'); //buttonタグ作成
+  doneButton.textContent = "完了"; //「完了」のテキスト追加
+  li.appendChild(doneButton); //liタグの末尾に追加
+
+  doneButton.addEventListener('click', ()=> {
+    li.classList.add("do"); //liタグにdoクラスを追加
+  });
 });
