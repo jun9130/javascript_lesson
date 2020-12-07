9d96dfea8b717ad572780d56290138d5f477b378
jun9130 <jun.nakama1030@gmail.com>
2020-12-07 14:14:00 +0900

    clickイベント

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const button = document.querySelector('button');
-button.addEventListener('click', ()=> {
-  const form = button.insertAdjacentHTML('afterend','<form action=""><textarea name="" id="" cols="30" rows="5" placeholder="レビューを書いてください" ></textarea><input type="submit"></form>');
+// const button = document.querySelector('button');
+// button.addEventListener('click', ()=> {
+//   const form = button.insertAdjacentHTML('afterend','<form action=""><textarea name="" id="" cols="30" rows="5" placeholder="レビューを書いてください" ></textarea><input type="submit"></form>');
   
-}, { once: true}); 
+// }, { once: true}); 
 
-// { once: true} イベント1回のみ発動
+const testElement = document.getElementById('test')
+
+
+testElement.addEventListener('click', function(){
+  this.textContent = "テストがクリックされました";
+  alert("テストがクリックされました");
+});
