f8e75537f50448f1b6af8cc5942405626eba4836
jun9130 <jun.nakama1030@gmail.com>
2020-12-09 14:04:21 +0900

    タブ切り替え

--- script.js
+++ script.js
@@ -10,4 +10,16 @@ const contents = document.querySelectorAll(".contents li");
 console.log(tabs);
 console.log(contents);
 
-
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener('click', function(e){
+    e.preventDefault();
+  for(let j = 0; j < tabs.length; j++) {
+    tabs[j].classList.remove("active");
+  } 
+  for(let j = 0; j < contents.length; j++) {
+    contents[j].classList.remove("active");
+  } 
+    this.classList.add('active')
+    contents[i].classList.add('active');
+  }); 
+}
