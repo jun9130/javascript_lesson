756478b4b692d546d735e5a72d057e5be531e899
jun9130 <jun.nakama1030@gmail.com>
2020-12-10 23:36:03 +0900

    条件分岐

--- script.js
+++ script.js
@@ -4,22 +4,33 @@
   
 // }, { once: true}); 
 
-const tabs = document.querySelectorAll(".tabs li a");
-const contents = document.querySelectorAll(".contents li");
+// const tabs = document.querySelectorAll(".tabs li a");
+// const contents = document.querySelectorAll(".contents li");
 
-console.log(tabs);
-console.log(contents);
+// console.log(tabs);
+// console.log(contents);
 
-for (let i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener('click', function(e){
-    e.preventDefault();
-  for(let j = 0; j < tabs.length; j++) {
-    tabs[j].classList.remove("active");
-  } 
-  for(let j = 0; j < contents.length; j++) {
-    contents[j].classList.remove("active");
-  } 
-    this.classList.add('active')
-    contents[i].classList.add('active');
-  }); 
+// for (let i = 0; i < tabs.length; i++) {
+//   tabs[i].addEventListener('click', function(e){
+//     e.preventDefault();
+//   for(let j = 0; j < tabs.length; j++) {
+//     tabs[j].classList.remove("active");
+//   } 
+//   for(let j = 0; j < contents.length; j++) {
+//     contents[j].classList.remove("active");
+//   } 
+//     this.classList.add('active')
+//     contents[i].classList.add('active');
+//   }); 
+// }
+
+const score = 10;
+
+if (score === 20) {
+  document.getElementById("test").textContent = "scoreは20です。";
+
+} else if (score === 10) {
+  document.getElementById("test").textContent = "scoreは10です。";
+}else {
+  document.getElementById("test").textContent = "scoreは20でも10ではありません";
 }
