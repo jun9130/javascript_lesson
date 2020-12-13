f3ce81328f3098c35664ecff49dbeb3e418ad0a7
jun9130 <jun.nakama1030@gmail.com>
2020-12-13 20:35:34 +0900

    関数の定義

--- script.js
+++ script.js
@@ -4,33 +4,17 @@
   
 // }, { once: true}); 
 
-// const tabs = document.querySelectorAll(".tabs li a");
-// const contents = document.querySelectorAll(".contents li");
+const tax = 1.1;
 
-// console.log(tabs);
-// console.log(contents);
 
-// for (let i = 0; i < tabs.length; i++) {
-//   tabs[i].addEventListener('click', function(e){
-//     e.preventDefault();
-//   for(let j = 0; j < tabs.length; j++) {
-//     tabs[j].classList.remove("active");
-//   } 
-//   for(let j = 0; j < contents.length; j++) {
-//     contents[j].classList.remove("active");
-//   } 
-//     this.classList.add('active')
-//     contents[i].classList.add('active');
-//   }); 
-// }
 
-const score = 10;
+function insertText(itemPrice, element) {
+  function calculation(price) {
+    return Math.floor(price * tax);
+  }
 
-if (score === 20) {
-  document.getElementById("test").textContent = "scoreは20です。";
-
-} else if (score === 10) {
-  document.getElementById("test").textContent = "scoreは10です。";
-}else {
-  document.getElementById("test").textContent = "scoreは20でも10ではありません";
+  document.getElementById(element).textContent = `${itemPrice}円の商品の税込価格は${calculation(itemPrice)}円です。`
 }
+
+insertText(100, "result");
+insertText(300, "result2");
