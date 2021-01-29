3eccf73d12b402632e64de5d2afb98aae47565c1
jun9130 <jun.nakama1030@gmail.com>
2021-01-29 22:15:50 +0900

    タスク追加後にinputの中を空にする

--- todo/todo.js
+++ todo/todo.js
@@ -1,11 +1,13 @@
 const addButton = window.document.getElementById('addButton');
-addButton.addEventListener('click', ()=> {  //アロー関数
+
+addButton.addEventListener('click', ()=> {  
   const input = window.document.querySelector('input'); //input要素取得
   const ul = window.document.querySelector('ul'); //ul要素取得
 
   const li = window.document.createElement('li'); //liタグ作成
   li.textContent = input.value; //入力されたテキストをliのテキストに反映
   ul.appendChild(li); //ulタグの最後に追加
+  input.value = ""; //inputの中を空に
 
   const doneButton = window.document.createElement('button'); //buttonタグ作成
   doneButton.textContent = "完了"; //「完了」のテキスト追加
