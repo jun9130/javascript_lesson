96724f5341e9cb36b3ad68d3425d41244f848e02
jun9130 <jun.nakama1030@gmail.com>
2021-02-04 15:53:46 +0900

    javascript 画像切り替え

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-// function tax(number) {
-//   return Math.floor(number * 1.1);
-// }
-
-// const tax = function tax(number) {
-//   return Math.floor(number * 1.1);
-// }
-
-// アロー関数
-// const tax = (number) => {
-//   return Math.floor(number * 1.1);
-// }
-
-// document.getElementById('test').textContent = tax(1000);
-
-// const multiplication = () => 5 * 20;
-// console.log(multiplication());
+const mainImage = document.getElementById('mainImage');
+const otherImage = document.querySelectorAll('.otherImage');
 
 
-
-// document.getElementById('test').addEventListener('click', function(){
-//   this.textContent = 'クリックされました';
-// })
-// 書き換え
-document.getElementById('test').addEventListener('click', () => {
-  document.getElementById('test').textContent = 'クリックされました';
-})
+otherImage.forEach(function(item, index) {
+  item.addEventListener('click', function() {
+    mainImage.src = this.src;
+  })
+});
