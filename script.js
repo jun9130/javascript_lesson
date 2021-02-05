e28e0a7b1d4be773bda300c46481a15663f57941
jun9130 <jun.nakama1030@gmail.com>
2021-02-05 22:59:00 +0900

    forEach

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const mainImage = document.getElementById('mainImage');
-const otherImage = document.querySelectorAll('.otherImage');
+const animals = ['犬', '猫', '牛', '馬', 'うさぎ' ];
+animalList = document.getElementById('animal');
 
+// for (let i = 0; i < animals.length; i++) {
+//   const li = document.createElement('li');
+//   li.textContent = animals[i];
+//   animalList.appendChild(li);
+// };
 
-otherImage.forEach(function(item, index) {
-  item.addEventListener('click', function() {
-    mainImage.src = this.src;
-  })
+animals.forEach((value, index, array) => {
+  const li = document.createElement('li');
+  li.textContent = `${index + 1}: ${value}`;
+  animalList.appendChild(li);
+  if (index + 1 === array.length) {
+    const liElement = document.createElement('li')
+    liElement.textContent = 'ループ終了!'
+    animalList.appendChild(liElement);
+  }
 });
