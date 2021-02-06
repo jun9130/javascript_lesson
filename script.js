f3b8a10903d362f5b44cb0e9c12ee8d4f1581b89
jun9130 <jun.nakama1030@gmail.com>
2021-02-06 23:14:39 +0900

    for forEach

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-document.getElementById("button").addEventListener('click', function(e){
-  e.preventDefault();
-  document.getElementById("box").classList.toggle("active");
+fruits = ["みかん", "いちご", "りんご", "ぶどう", "ばなな"]
+const fruitsList = document.getElementById('box')
+
+
+//for
+for (let i = 0; i < fruits.length; i++) {
+  const li = document.createElement('li');
+  li.textContent = fruits[i];
+  fruitsList.appendChild(li);
+}
+
+
+//forEach
+fruits.forEach((fruit) => {
+  const li = document.createElement('li');
+  li.textContent = fruit;
+  fruitsList.appendChild(li);
 });
