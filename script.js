1c682b050810e40283d624da32e2772fce6b4a4d
jun9130 <jun.nakama1030@gmail.com>
2021-02-06 15:58:53 +0900

    cssアニメーション transition

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-const animals = ['犬', '猫', '牛', '馬', 'うさぎ' ];
-animalList = document.getElementById('animal');
-
-// for (let i = 0; i < animals.length; i++) {
-//   const li = document.createElement('li');
-//   li.textContent = animals[i];
-//   animalList.appendChild(li);
-// };
-
-animals.forEach((value, index, array) => {
-  const li = document.createElement('li');
-  li.textContent = `${index + 1}: ${value}`;
-  animalList.appendChild(li);
-  if (index + 1 === array.length) {
-    const liElement = document.createElement('li')
-    liElement.textContent = 'ループ終了!'
-    animalList.appendChild(liElement);
-  }
+document.getElementById("button").addEventListener('click', function(e){
+  e.preventDefault();
+  document.getElementById("box").classList.toggle("active");
 });
