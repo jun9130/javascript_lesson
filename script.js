3103ebe871cacff1b79b0cf48e4b0a1a13437fba
jun9130 <jun.nakama1030@gmail.com>
2021-02-17 23:52:39 +0900

    消費税の計算

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.getElementById('button').addEventListener('click', function(){
-  const number = document.getElementById("number");
-  number.innerHTML = Number(number.innerHTML) + 1
-}, {once : true})
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const div = document.querySelector('div');
+
+button.addEventListener('click', function(){
+  taxIncluded = Math.floor(Number(input.value) * 1.1);
+  const p = document.createElement("p");
+  p.innerHTML = `${input.value}円の税込は${taxIncluded}円です。`;
+  input.value = ""
+  div.appendChild(p);
+})
