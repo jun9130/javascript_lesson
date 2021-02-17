24b8f3be639bb2dd0887f807c7b1a1601275160e
jun9130 <jun.nakama1030@gmail.com>
2021-02-17 14:54:08 +0900

    addEventListener()  once

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-const TargetElement = document.querySelectorAll(".animationTarget");
-console.log("画面の高さ", window.innerHeight)
-
-
-document.addEventListener("scroll",function() {
-  for (let i = 0; i < TargetElement.length; i++) {
-    const getElentDistance = TargetElement[i].getBoundingClientRect().top + TargetElement[i].clientHeight + .5
-    if (window.innerHeight > getElentDistance) {
-      TargetElement[i].classList.add("show");
-    }
-  }
-  });
+document.getElementById('button').addEventListener('click', function(){
+  const number = document.getElementById("number");
+  number.innerHTML = Number(number.innerHTML) + 1
+}, {once : true})
