7ed82cb577a50c9ba0b8c0ca95cf0606a8300a83
jun9130 <jun.nakama1030@gmail.com>
2021-02-25 15:39:13 +0900

    update

--- todo/todo.js
+++ todo/todo.js
@@ -2,44 +2,40 @@ const addButton = document.getElementById('addButton');
 
 addButton.addEventListener('click', ()=> {
   const input = document.querySelector('input');
-  const ul = document.querySelector('ul');
-  const li = document.createElement('li'); //liタグ作成
+  if (input.value != "") {
+    const ul = document.querySelector('ul');
+  
+    const li = document.createElement('li');
+    li.classList.add('todo');
+    
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type','checkbox');
+    
+    const task = document.createElement('span');
+    task.classList.add('todo-task');
+    task.textContent = input.value;
+    
+    const deleteButton = document.createElement('button'); 
+    deleteButton.textContent = "削除";
+    
+    ul.appendChild(li);
+    li.append(checkBox, task, deleteButton);
+    input.value = "";
 
-  li.textContent = input.value;
+    checkBox.addEventListener('change', ()=> {
+      if (checkBox.checked) {
+        task.classList.add('doneTodo');
+      }
+      else {
+        task.classList.remove('doneTodo');
+      }
+    })
+
+    deleteButton.addEventListener('click', ()=> {
+      li.style.display = "none" ;
+    })
+  }
+  else {
+    alert("タスクを入力してください");
+  }
 })
-
-// const doneTodo = document.querySelector('input[value="task"]')
-// doneTodo.addEventListener('change', ()=>{
-//   if (doneTodo.checked) {
-//     console.log('ok');
-//   }
-// })
-
-// addButton.addEventListener('click', ()=> {  
-//   const input = window.document.querySelector('input'); //input要素取得
-//   const ul = window.document.querySelector('ul'); //ul要素取得
-
-//   const li = window.document.createElement('li'); //liタグ作成
-//   li.textContent = input.value; //入力されたテキストをliのテキストに反映
-//   ul.appendChild(li); //ulタグの最後に追加
-//   input.value = ""; //inputの中を空に
-
-//   const doneButton = window.document.createElement('button'); //buttonタグ作成
-//   doneButton.textContent = "完了"; //「完了」のテキスト追加
-//   doneButton.classList.add("doneButton")
-//   li.appendChild(doneButton); //liタグの末尾に追加
-
-//   const deleteButton = window.document.createElement('button');
-//   deleteButton.textContent = "削除";
-//   deleteButton.classList.add("deleteButton");
-//   li.appendChild(deleteButton);
-
-//   doneButton.addEventListener('click', ()=> {
-//     li.classList.add("done"); //liタグにdoクラスを追加
-//   });
-
-//   deleteButton.addEventListener('click', ()=> {
-//     li.style.display = "none" ;
-//   });
-
-// });
