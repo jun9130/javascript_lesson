23b11c98b0c5bfe17116b090c9961c32472f5e16
jun9130 <jun.nakama1030@gmail.com>
2021-02-27 21:42:36 +0900

    lesson

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const div = document.querySelector('div');
+function double(num) {
+  return num * 2;
+}
 
-button.addEventListener('click', function(){
-  taxIncluded = Math.floor(Number(input.value) * 1.1);
-  const p = document.createElement("p");
-  p.innerHTML = `${input.value}円の税込は${taxIncluded}円です。`;
-  input.value = ""
-  div.appendChild(p);
-})
+double(10) 
