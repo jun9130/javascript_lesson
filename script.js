cd7489f1b3f3458cee509a9bd72c3630aa1d6146
jun9130 <jun.nakama1030@gmail.com>
2021-02-28 23:20:24 +0900

    Math.random Math.floor

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const randomNumber = Math.random(); //0~1未満の数値をランダムで生成
-document.getElementById('test').textContent = `今日のラッキーナンバーは${Math.floor(randomNumber * 5 + 1)}`;
+// const randomNumber = Math.random(); //0~1未満の数値をランダムで生成
+// document.getElementById('test').textContent = `今日のラッキーナンバーは${Math.floor(randomNumber * 5 + 1)}`;
 
 
-// const output = document.getElementById('output');
-// output.textContent = randomNumber;
+const output = document.getElementById('output');
+output.textContent = Math.round(6.2);
 
-const omikuji = ['大吉', '吉', '凶'];
-const omikujiRandomNumber = Math.floor(Math.random() * omikuji.length)
-document.getElementById('omikuji').textContent = omikuji[omikujiRandomNumber];
+// const omikuji = ['大吉', '吉', '凶'];
+// const omikujiRandomNumber = Math.floor(Math.random() * omikuji.length)
+// document.getElementById('omikuji').textContent = omikuji[omikujiRandomNumber];
 
 //じゃんけん
+
+console.log(Math.random() * 10);
+console.log(Math.floor(222.9))
+console.log(Math.floor(-19.9))
