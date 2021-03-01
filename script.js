559b2e6ec94493a6eb4d95e788ffd85bfdf209cd
jun9130 <jun.nakama1030@gmail.com>
2021-03-01 23:45:33 +0900

    lesson

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const randomNumber = Math.random(); //0~1未満の数値をランダムで生成
-const jyanken = ['グー', 'チョキ', 'パー']
-const jyankenRandom = Math.floor(randomNumber * jyanken.length );
+const today = new Date(); //インスタンス化
 
-document.getElementById('test').textContent = jyanken[jyankenRandom];
-//じゃんけん
+const string = Date();
+console.log(string === today);
+
+// // new Date(year, monthIndex [, day [, hours [, minutes [, seconds [, milliseconds]]]]])
+
+
+let birthday = new Date('December 17, 1995 03:24:00')
+let birthday2 = new Date('1995-12-17T03:24:00')
+let birthday3 = new Date(1995, 11, 17)            // the month is 0-indexed
+let birthday4 = new Date(1995, 11, 17, 3, 24, 0)
+
+console.log(birthday);
+console.log(birthday2);
+console.log(birthday3);
+console.log(birthday4);
