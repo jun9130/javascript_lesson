cba5d994f8823b5a6ac7d544d91302b9ccbf8ff6
jun9130 <jun.nakama1030@gmail.com>
2021-03-03 14:09:35 +0900

    lesson

--- script.js
+++ script.js
@@ -1,39 +1,12 @@
-const today = new Date(); //変数(インスタンス化)
-      days = ['日', '月', '火', '水', '木', '金', '土']
+const date = document.getElementById('date');
+const today = new Date();
+console.log(today);
+date.textContent = today.setDate(32);
 
 
-function outputDate(date) {
-  let outputText =  `${date.getFullYear()}年`
-  outputText +=  `${date.getMonth() + 1}月`
-  outputText +=  `${date.getDate()}日`
-  outputText +=  ` ${date.getHours()}時`
-  outputText +=  `${date.getMinutes()}分`
-  outputText +=  `${date.getSeconds()}秒`
-  outputText +=  `${date.getMilliseconds()}ミリ秒`
-  outputText +=  ` ${days[date.getDay()]}曜日`
-  return outputText;
-}
+n = 3
+console.log(typeof n);
 
-
-console.log(today)
-console.log(today.getFullYear())
-console.log(today.getMonth() + 1)
-console.log(today.getDate())
-console.log(today.getHours())
-console.log(today.getMinutes())
-console.log(today.getSeconds())
-console.log(today.getMilliseconds())
-console.log(days[today.getDay()])
-
-
-document.getElementById('date').textContent = `今は${outputDate(today)}`
-
-const targetDate = new Date(2021, 7 - 1, 23);
-console.log(Math.ceil((targetDate - today) / (1000 * 60 * 60 * 24) )) ;
-
-
-today.setDate(today.getDate() + 1);
-document.getElementById('tomorrow').textContent = `明日の今は${outputDate(today)}`
-
-document.getElementById('countDown').textContent = `あと${Math.ceil((targetDate - today) / (1000 * 60 * 60 * 24) )} 日`
-
+const day = new Date(2021, n - 1, 3);
+console.log(day);
+console.log(day.setDate(32));
