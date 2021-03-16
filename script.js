fa93e90c5ad4b47a1b47985481f3f93535c948ae
jun9130 <jun.nakama1030@gmail.com>
2021-03-16 14:18:15 +0900

    lesson

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('input');
-const button = document.querySelector('button');
+const button = document.querySelectorAll('button');
 
-button.addEventListener("click", function(){
-  console.log("ok");
-});
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', function(){
+    input.value += String(button[i].textContent);
+  });
+}
