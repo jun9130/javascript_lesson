3afd8f9506c31cc56bd3bc054ebc65f4cd1a3779
jun9130 <jun.nakama1030@gmail.com>
2021-03-18 14:10:18 +0900

    lesson

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('input');
 const button = document.querySelectorAll('button.number');
 const clear = document.getElementById('clear');
+const back = document.getElementById('back');
 const calculation = document.getElementById('calculation');
 
 
@@ -8,10 +9,15 @@ for (let i = 0; i < button.length; i++) {
   button[i].addEventListener('click', function(){
     input.value += button[i].textContent;
   });
+  
 }
 
 clear.addEventListener('click', function(){
   input.value = " ";
+})
+
+back.addEventListener('click', function(){
+  input.value = input.value.slice(0, input.value.length - 1); 
 })
 
 calculation.addEventListener('click', function(){
@@ -19,4 +25,6 @@ calculation.addEventListener('click', function(){
   const result = new Function( 'return ' + inputData )
   input.value = result().toString()
 });
+
+
 
