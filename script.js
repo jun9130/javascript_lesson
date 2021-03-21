7558b1f6791e6fdd0c3b4324646f6657e1687997
jun9130 <jun.nakama1030@gmail.com>
2021-03-22 07:13:43 +0900

    mapメソッド

--- script.js
+++ script.js
@@ -1,39 +1,5 @@
-const input = document.querySelector('input');
-const button = document.querySelectorAll('button.number');
-const clear = document.getElementById('clear');
-const back = document.getElementById('back');
-const calculation = document.getElementById('calculation');
-const taxIncluded = document.getElementById('taxIncluded');
-const excludingTax = document.getElementById('excludingTax');
+const array = [1, 2, 3];
 
+const tenTimes = array.map(num => num * 10);
 
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener('click', function(){
-    input.value += button[i].textContent;
-  });
-  
-}
-
-clear.addEventListener('click', function(){
-  input.value = " ";
-})
-
-back.addEventListener('click', function(){
-  input.value = input.value.slice(0, input.value.length - 1); 
-})
-
-calculation.addEventListener('click', function(){
-  const inputData = input.value
-  const result = new Function( 'return ' + inputData )
-  input.value = parseFloat(result());
-});
-
-taxIncluded.addEventListener('click', function(){
-  input.value = Math.round(parseInt(input.value) * 1.1);
-})
-
-excludingTax.addEventListener('click', function(){
-  input.value = Math.round(parseInt(input.value) / 1.1);
-})
-
-
+console.log(tenTimes);
