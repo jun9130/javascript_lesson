55ebb30dcbbf07aa2101bc0d672168e125e42f47
jun9130 <jun.nakama1030@gmail.com>
2021-03-23 15:03:55 +0900

    mapメソッド filterメソッド

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-const canDrinkAlcohol = (age) => {
-  if (age >= 20) {
-    return true
-  } 
-    return false
-}
+const array1 = [1, 4, 9, 16];
 
-console.log(canDrinkAlcohol(20));
+const map1 = array1.map(x => x * 2);
 
-const test = document.getElementById('test')
-const select = document.getElementById('select');
+console.log(map1)
 
 
+const map2 = array1.map(function(x) {
+  return x * 2
+});
+
+console.log(map2);
 
 
-const needUmbrella = (weather) => {
-  if (weather === 'sunny') {
-    return 'no'
-  } else if (weather === 'cloudy') {
-    return 'maybe'
-  } else if (weather === 'rainy') {
-    return 'yes'
-  }
-}
 
-console.log(needUmbrella("rainy"));
+const fruits = ["apple", "melon", "banana", "lemon"];
+const filter1 = fruits.filter(word => word.match(/[a]/));
+
+console.log("aの文字が入っているfruits", filter1);
