e5823a6ccf3bedfa3ecb62cf4c4ffa4168ebb9a3
jun9130 <jun.nakama1030@gmail.com>
2021-03-23 23:00:47 +0900

    filter

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-const array1 = [1, 4, 9, 16];
+const num = [10, 20, 30];
 
-const map1 = array1.map(x => x * 2);
+const result = num.filter(n => n * 10 > 500);
 
-console.log(map1)
-
-
-const map2 = array1.map(function(x) {
-  return x * 2
-});
-
-console.log(map2);
-
-
-
-const fruits = ["apple", "melon", "banana", "lemon"];
-const filter1 = fruits.filter(word => word.match(/[a]/));
-
-console.log("aの文字が入っているfruits", filter1);
+console.log(result);
