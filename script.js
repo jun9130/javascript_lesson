f5389ee6001b3e499f079bcf9151fbcd86698b19
jun9130 <jun.nakama1030@gmail.com>
2021-03-25 15:04:31 +0900

    lesson

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const num = [10, 20, 30];
-
-const result = num.filter(n => n * 10 > 500);
-
-console.log(result);
+const today = new Date();
+console.log(today.getDate());
+console.log(today.getDay());
+console.log(today.getFullYear());
+console.log(today.getMonth());
