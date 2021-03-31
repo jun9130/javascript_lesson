6d9813898bc056de6ac17be536ed80cc355a54ea
jun9130 <jun.nakama1030@gmail.com>
2021-03-31 14:08:18 +0900

    正規表現

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const today = new Date();
-console.log(today.getDate());
-console.log(today.getDay());
-console.log(today.getFullYear());
-console.log(today.getMonth());
+const zip_pattern = /^\d{3}-?\d{4}/g;
+let zip = '123-4567';
+
+if (zip_pattern.test(zip) === true) {
+  console.log("正しく入力されました");
+} else {
+  console.log("郵便番号は xxx-xxxxの書式で入力してください");
+}
