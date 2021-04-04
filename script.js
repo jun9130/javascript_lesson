d7cdcc4b5b5058aa26dbcb52180797a651a534aa
jun9130 <jun.nakama1030@gmail.com>
2021-04-04 19:19:42 +0900

    JSON

--- script.js
+++ script.js
@@ -9,5 +9,21 @@ const images = [
     "name": "name02",
     "caption": "コメント2"
   },
-
 ];
+
+let img;
+let caption;
+let div;
+img = document.createElement('img');
+img.setAttribute('src', images[0].path);
+caption = document.createElement('div');
+caption.className = 'inner';
+caption.innerHTML = '<p>' + images[0].caption + '<span>' + images[0].name + '</span>' +  '</p>';
+div = document.createElement('div');
+div.className = 'photo';
+div.appendChild(img);
+div.appendChild(caption);
+
+
+document.getElementById('img_unit').appendChild(div);
+
