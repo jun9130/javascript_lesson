87e57f35a85484f76bd6c7802e6d99d9d76602aa
jun9130 <jun.nakama1030@gmail.com>
2021-04-05 15:59:54 +0900

    連想配列

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-const images = [
-  {
-    "path": "https://placehold.jp/35c3e6/ffffff/500x500.png",
-    "name": "name01",
-    "caption": "コメント1"
-  },
-  {
-    "path": "https://placehold.jp/e529f2/ffffff/500x500.png",
-    "name": "name02",
-    "caption": "コメント2"
-  },
-  {
-    "path": "https://placehold.jp/388210/ffffff/500x500.png",
-    "name": "name03",
-    "caption": "コメント3"
-  },
-];
-
-let img;
-let caption;
-let div;
-
-for (let i = 0; i < images.length; i++) {
-  img = document.createElement('img');
-  img.setAttribute('src', images[i].path);
-  caption = document.createElement('div');
-  caption.className = 'inner';
-  caption.innerHTML = '<p>' + images[i].caption + '<span>' + images[i].name + '</span>' + '</p>';
-  div = document.createElement('div');
-  div.className = 'photo';
-  div.appendChild(img);
-  div.appendChild(caption);
-  document.getElementById('img_unit').appendChild(div);
+const item = {
+  name: "パソコン",
+  price: 100000,
+  sale: true,
+  parts: ["CPU", "メモリ", "HDD"],
+  a: {
+    b: "テスト"
+  }
 }
 
+document.getElementById("name").textContent = `商品名は${item["name"]}です。`
+document.getElementById("price").textContent = `値段は${item.price}円です。`
+
+// item.sale = false;
+
+if (item.sale)
+document.getElementById("sale").textContent = `${item.name}はセール中です.
+`
+
+let texts = "";
+for (let i = 0; i < item.parts.length; i++ ) {
+  texts += item.parts[i] + " ";
+}
+document.getElementById("parts").textContent = `${item.name}のパーツは${texts}です.`
+
+document.getElementById("test").textContent = item["a"]["b"];
+
+console.log(item);
+console.table(item);
