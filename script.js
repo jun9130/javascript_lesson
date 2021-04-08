edf68fec0a9aca0f61da7a71d6e094ca923a08f7
jun9130 <jun.nakama1030@gmail.com>
2021-04-08 13:50:45 +0900

    lesson

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth() + 1;
+const date = new Date(); // 今日
+const year = date.getFullYear(); // 今年
+const month = date.getMonth() + 1; // 今月
+const firstDay = new Date(year, month - 1, 1); // 今月の一日
+const lastDay = new Date(year, month, 0); // 今月が何日まであるか
+const firstDayWeek = firstDay.getDay(); // 今月の一日の曜日
+const lastDayCount = lastDay.getDate(); // 今月の末日
+const weeks = ['日', '月', '火', '水', '木', '金', '土']
 
-const firstDay = new Date(year, month - 1, 1); // 月の最初の日を取得
-const lastDay = new Date(year, month, 0); // 月の最後の日を取得
 
-console.log(month);
+
+
+console.log(`${month}月`);
 console.log(firstDay);
 console.log(lastDay);
+console.log(`今月は${lastDayCount}日まで`);
+console.log(weeks[firstDayWeek]);
 
 
-for (let i = 1; i <= 3; i++) {
-  console.log(i * 10);
-}
-
-const array = [1, 2, 3];
-
-array.map(x => console.log(x * 10));
-
-
-
-
-
-// const endDayCount = endDate.getDate() // 月の末日
-// const startDay = startDate.getDay() // 月の最初の日の曜日を取得
-
-// const weeks = ['日', '月', '火', '水', '木', '金', '土']
 
 // let dayCount = 1 // 日にちのカウント
-// let calendarHtml = '' // HTMLを組み立てる変数
+let calendarHtml = '' // HTMLを組み立てる変数
 
-// calendarHtml += '<h1>' + year  + '/' + month + '</h1>'
-// calendarHtml += '<table>'
+calendarHtml += `<h1>${year}/${month}</h1>`
+console.log(calendarHtml);
+
+calendarHtml += '<table>'
+
+console.log(calendarHtml);
 
 // // 曜日の行を作成
 // for (let i = 0; i < weeks.length; i++) {
 //     calendarHtml += '<td>' + weeks[i] + '</td>'
 // }
+
+
+
+
+
+
+
+
 
 // for (let w = 0; w < 6; w++) {
 //     calendarHtml += '<tr>'
