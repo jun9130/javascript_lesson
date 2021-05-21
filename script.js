635031f549238615b3bc841b6744e3848c9f0d39
jun9130 <jun.nakama1030@gmail.com>
2021-05-21 14:31:50 +0900

    lesson

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function add(a, b) {
 
 const test = add(2, 3);
 console.log(test);
+
+function argumentsToArray(x, y) {
+  return [x, y];
+}
+
+console.log(argumentsToArray(1, 2)); 
+console.log(argumentsToArray(1));
