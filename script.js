817256fb11ff9a788813d25f7d201002394345ee
jun9130 <jun.nakama1030@gmail.com>
2021-05-24 13:44:35 +0900

    filter

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const array = [1, 9, 10, 19];
+// const array = [1, 9, 10, 19];
+// const result = array.map(x => x * 10);
+// console.log(result);
 
-const result = array.map(x => x * 10);
 
-console.log(result);
+const test = [1, 3, 33, 5, 6, 9, 10, 22];
+const result2 = test.filter(num => num > 10);
+console.log(result2);
